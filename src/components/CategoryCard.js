763abdf3965513d1639fbbcf151f3f9ea5da4817
bilalseen <bilalsen2010@gmail.com>
2024-04-08 02:11:44 +0300

    Refactor CategoryCard component to improve UI and add functionality

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,55 +1,45 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  Pressable,
-  ToastAndroid,
-  Vibration,
-  Dimensions,
-  Image,
-} from "react-native";
+import { View, Text, Pressable, Dimensions, Image } from "react-native";
 import React from "react";
 export default function CategoryCard({ category, navigateToMeals }) {
   const { strCategory } = category;
 
   return (
     <Pressable onPress={navigateToMeals}>
-      <View style={styles.container}>
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#fff",
+          height: Dimensions.get("window").height / 9,
+          width: Dimensions.get("window").width / 3,
+          margin: 10,
+          marginTop: 50,
+          gap: 20,
+          borderRadius: 10,
+          elevation: 5,
+        }}
+      >
         <Image
           source={{ uri: category.strCategoryThumb }}
-          style={styles.image}
+          style={{
+            width: 100,
+            height: Dimensions.get("window").height / 10 - 10,
+            position: "absolute",
+            top: -40,
+            borderRadius: 50,
+          }}
         />
-        <Text style={styles.text}>{strCategory}</Text>
+        <Text
+          style={{
+            position: "absolute",
+            bottom: 20,
+            fontSize: 16,
+            fontWeight: "bold",
+          }}
+        >
+          {strCategory}
+        </Text>
       </View>
     </Pressable>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
-    height: Dimensions.get("window").height / 9,
-    width: Dimensions.get("window").width / 3,
-    margin: 10,
-    marginTop: 50,
-    gap: 20,
-    borderRadius: 10,
-    borderColor: "#BCAAA4",
-    borderWidth: 1,
-  },
-  text: {
-    position: "absolute",
-    bottom: 20,
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  image: {
-    width: 100,
-    height: Dimensions.get("window").height / 10 - 10,
-    position: "absolute",
-    top: -40,
-    borderRadius: 50,
-  },
-});
