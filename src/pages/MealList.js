e8000ac6515a6e7616a91144be715b9415eb062a
bilalseen <bilalsen2010@gmail.com>
2024-04-08 03:12:21 +0300

    Refactor MealList.js to add LottieView animation for loading indicator and improve UI

--- src/pages/MealList.js
+++ src/pages/MealList.js
@@ -1,10 +1,15 @@
-import { View, Text, FlatList } from "react-native";
+import { View, FlatList, Dimensions } from "react-native";
 import React from "react";
 import { useFetch } from "../hooks/useFetch/useFetch";
 import MealCard from "../components/MealCard";
+import LottieView from "lottie-react-native";
 
 const MealList = ({ route, navigation }) => {
   const { categoryName } = route.params;
+
+  const deviceWidth = Dimensions.get("window").width;
+  const deviceHeight = Dimensions.get("window").height;
+
   const { data, error, loading } = useFetch(
     `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
   );
@@ -15,8 +20,20 @@ const MealList = ({ route, navigation }) => {
 
   if (loading) {
     return (
-      <View>
-        <Text>Loading...</Text>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          height: deviceHeight,
+        }}
+      >
+        <LottieView
+          source={require("../../assets/animations/loading.json")}
+          style={{ width: 400, height: 400 }}
+          autoPlay
+          loop
+        />
       </View>
     );
   }
